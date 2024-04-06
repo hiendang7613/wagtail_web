607c1f7f85e66363260152f7619b91836872e408
hiendang7613 <18120363@student.hcmus.edu.vn>
2024-04-06 18:38:40 +0700

    fdsafasdf

---
+++ deepwhaleweb/deepwhaleweb/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-&^&az^9hqvcd+!87z2)e#t(wc#7v$qsg!!^v@p!yfan%5_yws3"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
